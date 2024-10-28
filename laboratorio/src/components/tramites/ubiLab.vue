890a18f83023bac9c43990e9d4652c2548c9a29a
Michgarcia1 <sarahiggutierrez422@gmail.com>
2024-10-28 09:29:11 -0600

    ubiLab.vue

--- laboratorio/src/components/tramites/ubiLab.vue
+++ laboratorio/src/components/tramites/ubiLab.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-    hola
+  <div class="bg-white q-pa-lg"
+  :style="$q.screen.lt.sm ? {} : { height: '550px', width: '1200px' }">
+
+    <div class="text-h6 q-pa-md" style="font-weight: bold">
+      Ubicación del Laboratorio
+    </div>
+
+    <div id="map" style="height: calc(100% - 64px);"></div>
   </div>
 </template>
+
+<script setup lang="ts">
+
+
+
+</script>
+
+<style scoped>
+#map {
+  width: 100%;
+  height: 100%;
+}
+</style>
