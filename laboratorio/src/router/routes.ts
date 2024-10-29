fa6d57d9f8ebbaf17842bd86693984069ef9f6a4
Michgarcia1 <sarahiggutierrez422@gmail.com>
2024-10-29 17:39:37 -0600

    agendar cita

--- laboratorio/src/router/routes.ts
+++ laboratorio/src/router/routes.ts
@@ -26,6 +26,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/tramites/clinicaExam.vue') }],
   },
+   {
+    path: '/proceso-de-compra',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/procesoCompra.vue') }],
+  },
   {
     path: '/resultados',
     component: () => import('layouts/MainLayout.vue'),
