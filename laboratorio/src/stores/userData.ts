82245fd8800b58356d563c5ecc3529f55ca8a4d7
Michgarcia1 <sarahiggutierrez422@gmail.com>
2024-11-04 09:45:57 -0600

    back

--- laboratorio/src/stores/userData.ts
+++ laboratorio/src/stores/userData.ts
@@ -3,7 +3,9 @@ import { defineStore } from 'pinia';
 export const userData = defineStore('userData', {
   state: () => ({
     email: '',
-    password: ''
+    password: '',
+    access_token: '',
+    refresh_token: '',
   }),
   actions:{
     setEmail(email:string){
@@ -11,6 +13,12 @@ export const userData = defineStore('userData', {
     },
     setPassword(password:string){
       this.password = password
+    },
+    setAccessToken(access_token:string){
+      this.access_token = access_token
+    },
+    setRefreshToken(refresh_token:string){
+      this.refresh_token = refresh_token
     }
   }
 })
