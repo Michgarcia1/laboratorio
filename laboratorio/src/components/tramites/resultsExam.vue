19137d11bf8e12ad5c81b7e48bb612c43528eb86
Michgarcia1 <sarahiggutierrez422@gmail.com>
2024-11-04 09:33:35 -0600

    consulta resultados

--- laboratorio/src/components/tramites/resultsExam.vue
+++ laboratorio/src/components/tramites/resultsExam.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    hola
+  <div class="bg-white q-pa-lg"
+  :style="$q.screen.lt.sm ? {height: '750px', width: '380px'} : { height: '550px', width: '1200px' }">
+
+    <div class="text-h6 q-pa-md" style="font-weight: bold">
+      Consulta tus resultados
+    </div>
+
   </div>
+
 </template>
 <script setup lang="ts">
 </script>
