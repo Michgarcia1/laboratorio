1e6754a59b961865bf9142bb6b2158d3aa224f93
martinDTV <martin.delatorre@monterrey.gob.mx>
2024-11-29 18:22:58 -0600

    mini changes

--- laboratorio/src/components/componenteUbicacion.vue
+++ laboratorio/src/components/componenteUbicacion.vue
@@ -7,22 +7,52 @@ import { onMounted } from 'vue';
 import 'ol/ol.css';
 import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import { Feature } from 'ol';
+import Point from 'ol/geom/Point';
 import OSM from 'ol/source/OSM';
 import { fromLonLat } from 'ol/proj';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
 
 onMounted(() => {
+  const coordinates = fromLonLat([-100.172028, 25.682861]);
+
+  const pointFeature = new Feature({
+    geometry: new Point(coordinates),
+  });
+
+
+  pointFeature.setStyle(
+    new Style({
+      image: new Icon({
+        src: 'https://cdn-icons-png.flaticon.com/512/484/484167.png',
+        scale: 0.08,
+      }),
+    })
+  );
+
+    const vectorSource = new VectorSource({
+    features: [pointFeature],
+  });
+
+  const vectorLayer = new VectorLayer({
+    source: vectorSource,
+  });
+
   new Map({
     target: 'map',
     layers: [
       new TileLayer({
         source: new OSM(),
       }),
+      vectorLayer,
     ],
     view: new View({
-      center: fromLonLat([-99.1332, 19.4326]),
-      zoom: 2,
+      center: coordinates,
+      zoom: 15,
     }),
   });
 });
-
 </script>
